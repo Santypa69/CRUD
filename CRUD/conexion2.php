303006b3d4db78993155bd66d7635ce99eceb8d4
Santypa69 <paredess284@gmail.com>
2024-11-12 19:41:56 -0500

    conexion2.php

--- CRUD/conexion2.php
+++ CRUD/conexion2.php
@@ -1,16 +1,17 @@
 <?php
-$servidor='localhost';
-$basedatos='compania';
-$usuario='paquito';
-$contrasena='';
+// Obtener variables de entorno
+$servidor = getenv('DB_HOST');
+$basedatos = getenv('DB_NAME');
+$usuario = getenv('DB_USER');
+$contrasena = getenv('DB_PASSWORD');
 
-$conexion2 = new mysqli($servidor,$usuario,$contrasena,$basedatos);
-if ($conexion2->connect_errno)
-{
-	echo"error de conexion verifique sus datos ";
-}
-else 
-{
-	echo "<h1>Conectado listo para usar la base de datos de almacen</h1>";
+// Establecer conexión a la base de datos
+$conexion2 = new mysqli($servidor, $usuario, $contrasena, $basedatos);
+
+// Verificar la conexión
+if ($conexion2->connect_errno) {
+    echo "Error de conexión: Verifique sus datos.";
+} else {
+    echo "<h1>Conectado a la base de datos de almacén</h1>";
 }
 ?>
